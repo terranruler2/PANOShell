657958ed27645dbaa2759cb67a48d81c7b363af9
Justin Grote <justingrote+github@gmail.com>
2017-01-05 12:00:25 -0800

    Added Credential, API, and SSL Handling

--- Public/Invoke-APIRequest.ps1
+++ Public/Invoke-APIRequest.ps1
@@ -10,46 +10,87 @@ This command is the core of the PANOS Module and is used by nearly every other c
     param (
         #Hostname or IP Address of PANOS (Firewall, Panorama, Collector, etc.) device to issue the request.
         #If not specified it will use the global session information
-        [String]$Hostname,
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)][String[]]$Hostname,
 
         #A hashtable list of arguments for the command, such as Type, Action, and XPath/CMD
         $ArgumentList = @{},
 
-        #HTTP Method to Use. Defaults to GET, but POST is sometimes required for some commands that upload files
-        [ValidateSet("GET","POST")]$HTTPMethod = "GET"
+        #HTTP Method to Use. Defaults to POST. API Key Requests are always done as POST regardless to obfuscate the credentials used
+        #WARNING: GET WILL SHOW THE ENTIRE API REQUEST URI IN VERBOSE LOGS, INCLUDING API KEYS
+        [ValidateSet("GET","POST")]$HTTPMethod = "POST",
+
+        #Ignore SSL Errors. Not recommended for production use but useful for testing or systems with self-signed certificates
+        #WARNING: Due to the nature of validation aching, if you specify this once it may take effect for future commands in the session.
+        [Switch]$Insecure,
+
+        #If a credential is specified, use those credentials for the request.
+        #This is a one-time action and is not saved, Use Connect-PANOSDevice to persist login information
+        [PSCredential]$Credential
     )
 
-    $RequestParams = @{
-        Method = $HTTPMethod
-        #Passthrough Common Parameters to Request
-        Verbose = ($PSBoundParameters.Verbose -eq $true)
-    }
-
-    #Retrieve API credentials unless 'keygen' action is specified or an API key was manually provided to the command
-    if ($ArgumentList.Type -notmatch 'keygen') {
-        if (! $ArgumentList.key) {
-
-
-        }
-    }
-
-
-    if ($PSCmdlet.ShouldProcess($HostNameItem,"Invoking API $($ArgumentList.Type) Request")) {
-        #Use TLS1.2 by Default. TLS1.0 might be disabled on some Palo Alto Security Profiles.
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-        $APIResponse = (Invoke-RestMethod @RequestParams -Uri "https://$Hostname/api" -Body $ArgumentList).response
-
-        switch ($APIResponse.status) {
-            'Error' {
-                write-error ("$Hostname responded with Error " + $APIResponse.code + ": " + $APIResponse.result.msg)
+    process {
+        foreach ($HostnameItem in $Hostname) {
+            $RequestParams = @{
+                Method = $HTTPMethod
+                #Passthrough Common Parameters to Request
+                Verbose = ($PSBoundParameters.Verbose -eq $true)
+                Insecure = $Insecure
             }
-            'Success' {
-                $APIResponse.result
+
+            #Retrieve API credentials unless 'keygen' action is specified or an API key was manually provided to the command
+            if (($ArgumentList.Type -notlike 'keygen') -and (! $ArgumentList.key)) {
+                #If Credentials are present, use those to fetch an API key for a one-time operation
+                if ($Credential) {
+                    write-verbose "PANOS Credential specified for $HostnameItem, fetching API Key"
+                    $APIRequestParams = @{
+                        type="keygen"
+                        user=$Credential.GetNetworkCredential().UserName
+                        password=$Credential.GetNetworkCredential().Password
+                    }
+                    #Adjust the action but preserve all other parameters such as verbose, insecure, etc.
+                    $PSBoundParameters.Remove("ArgumentList") | out-null
+                    $PSBoundParameters.Remove("Hostname") | out-null
+                    $ArgumentList.key = (Invoke-APIRequest @PSBoundParameters -Hostname $HostNameItem -ArgumentList $APIRequestParams).key
+                } else {
+                    throw "No API Key Found"
+                    #TODO: Attempt the connect using any in-memory connection info
+                    #Connect-Device -Hostname $HostNameItem
+                }
             }
-            default {
-                write-error "$Hostname responded with unknown status" $APIResponse.status
+
+            #If a keygen is requested, always use POST to obfuscate the credentials used from verbose LOGS
+            if ($ArgumentList.type -like 'keygen') { $ArgumentList.method = "POST" }
+
+            if ($PSCmdlet.ShouldProcess($HostNameItem,"Invoking API $($ArgumentList.Type) Request")) {
+                #Use TLS1.2 by Default. TLS1.0 might be disabled on some Palo Alto Security Profiles.
+                [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                try {
+                    $APIResponse = (Invoke-RestMethod @RequestParams -Uri "https://${HostnameItem}/api" -Body $ArgumentList).response
+                }
+                catch [System.Net.WebException] {
+                    $lastError = $_
+                    if ($lastError.exception.status -match 'TrustFailure') {
+                        write-error "Could not establish trust relationship for the SSL/TLS secure channel to $HostNameItem. Try running Invoke-APIRequest again with the -insecure parameter"
+                        continue
+                    } else {
+                        #Pass through the error
+                        $lastError
+                    }
+                }
+                #>
+
+                switch ($APIResponse.status) {
+                    'Error' {
+                        write-error ("$HostnameItem responded with Error " + $APIResponse.code + ": " + $APIResponse.result.msg)
+                    }
+                    'Success' {
+                        $APIResponse.result
+                    }
+                    default {
+                        write-error "$HostnameItem responded with unknown status" $APIResponse.status
+                    }
+                }
             }
-        }
-    }
-}
+        } #ForEach
+    } #Process
+} #Function
