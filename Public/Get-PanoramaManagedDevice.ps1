6100611a079574369a2c726d78c24843d17ea4dc
Justin Grote <justingrote+github@gmail.com>
2017-01-06 18:48:11 -0800

    Added Get-PanoramaManagedDevice

---
+++ Public/Get-PanoramaManagedDevice.ps1
@@ -0,0 +1,28 @@
+function Get-PanoramaManagedDevice {
+<#
+.SYNOPSIS
+Returns a list of devices that Panorama manages
+.NOTES
+Only works on Panorama devices
+#>
+
+    [CmdletBinding()]
+    param (
+        #Only return devices which are currently connected to the firewall
+        [Switch]$Connected
+    )
+
+    begin {
+        $cmdXML = Add-PANOSCommandElement "show devices"
+        if ($Connected) {$cmdxml = Add-PANOSCommandElement "connected" $cmdXML}
+        else {$cmdxml = Add-PANOSCommandElement "all" $cmdXML}
+    }
+
+    process {
+        $APIResponse = Invoke-APIRequest -Verbose -ArgumentList @{
+            type="op"
+            cmd=$cmdXML.innerxml
+        }
+        $APIResponse.devices.entry
+    }
+}
