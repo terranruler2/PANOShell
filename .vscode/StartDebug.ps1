302aa33a4a07cfcf6a771e048060b04146c729d4
Justin Grote <justingrote+github@gmail.com>
2017-01-10 08:47:15 -0800

    More Command Syntax Processing Added

--- .vscode/StartDebug.ps1
+++ .vscode/StartDebug.ps1
@@ -1,8 +1,14 @@
+$verbosepreference = "Continue"
 import-module PanoShell
 connect-panosdevice scagpano -quiet
-#Should Error One Way
-invoke-panosapirequest -url '/api/?type=op&cmd=<show><devices></devices></show>' -rawxml
-#Should Error Another Way
-invoke-panosapirequest -url '/api/?type=asdfasdf'
-(Invoke-PANOSOperationalCommand -command "show system info" -target 'SCAGFW*')
-Invoke-panosapirequest -argumentlist @{type='op';action='complete';command='show devicegroups name'} -rawxml -verbose
+write-host -foreground cyan 'Should Error with "devices is missing"'
+#invoke-panosapirequest -url '/api/?type=op&cmd=<show><devices></devices></show>' -rawxml
+write-host -foreground cyan 'Should Error with "illegal value for parameter"'
+#invoke-panosapirequest -url '/api/?type=asdfasdf'
+write-host -foreground cyan 'Should Return System Information for two devices'
+#Invoke-PANOSOperationalCommand -command "show system info" -target 'SCAGFW*'
+#write-host -foreground cyan 'Should Error with "no completions available"'
+#Invoke-panosapirequest -argumentlist @{type='op';action='complete';command='show devicegroups name'} -rawxml
+
+write-host 'Testing advanced CLI layout'
+Invoke-PANOSOperationalCommand -target scagfw02a 'test security-policy-match from=Trusted to=Untrusted source=172.22.17.1 destination="4.2.2.2" destination-port=443 protocol=6 show-all=yes'
--- Private/Add-PANOSCommandElement.ps1
+++ Private/Add-PANOSCommandElement.ps1
@@ -9,30 +9,126 @@ Currently only works for simple commands with no attributes or parameters
         [Parameter(Mandatory,ValueFromPipeline)][String[]]$Command,
         #XML Object to append to. If not specified,creates a new one.
         [XML]$xmlObject = (New-Object System.XML.XmlDocument)
-
     )
 
-    begin {
-        $XPATHDeepestChildElement = '//*[not(../*/*)]'
-    }
+    $XPATHDeepestChildElement = '//*[not(../*/*)]'
+    $XPATHCommandRoot = '//*[@CommandRoot]'
 
-    process {
-        foreach ($CommandItem in $Command) {
-            #Split into unquoted words (arguments) and quoted phrases (values)
+    #region Command Parsing Regex
+    $REGEXNameValuePairQuoted = @'
+(\w+?\=["'][^"']+?["'])
+'@
+    $REGEXNameValuePair = @'
+(\w+?\=[^"' ]+)
+'@
+    $REGEXValue = @'
+(["'][^"']+?["'])
+'@
+    $REGEXArgument = @'
+([^"' ]+)
+'@
+    #Construct Regex Command Parser
+    $REGEXCmdParser = [RegEx](
+        $REGEXNameValuePairQuoted + '|' +
+        $REGEXNameValuePair + '|' +
+        $REGEXValue + '|' +
+        $REGEXArgument
+    )
 
-            $CommandItem.split(' ') | foreach-object {
-                write-verbose "Processing $PSItem"
-                if ($xmlObject.HasChildNodes) {
-                    $xmlObject.SelectSingleNode($XPATHDeepestChildElement).appendchild($xmlObject.CreateElement($PSItem)) | out-null
+    #Special Delimiter for the root of the command
+    $REGEXRootCommandDelimiter = '^[#^!:@>]$'
+
+    #Combine the string if it was provided as an array or from the pipeline
+    $Command = "$Command"
+
+    foreach ($CommandItem in $REGEXCmdParser.matches($Command).value) {
+        #Process the XML depending on what type of command line syntax was provided
+        switch -regex ($CommandItem) {
+            $REGEXRootCommandDelimiter {
+                #This specifies if we should mark the deepest element as a parent, so future entries can behave like
+                #The PAN-OS CLI
+                if (!$xmlObject.SelectSingleNode($XPATHCommandRoot)) {
+                    $commandRoot = $XmlObject.SelectSingleNode($XPATHDeepestChildElement)
+                    $commandRoot.setAttribute("CommandRoot","true")
                 } else {
-                    $xmlObject.appendchild($xmlObject.CreateElement($PSItem)) | out-null
+                    $commandRoot = $xmlObject.SelectSingleNode($XPATHCommandRoot)
                 }
+                break
             }
+            $REGEXNameValuePairQuoted {
+                write-debug "RegexNameValuePairQuoted: $CommandItem"
+                #Same as REGEXNameValuePair but strip quotes
+                #TODO: Deduplicate RegexNameValuePair Logic to a function
+                #Strip out the quotes
+                $commandItem = $commandItem.replace('"','')
+
+                #only split on first equals in case there is one in the value
+                $splitCommandItem = $CommandItem -split '=',2
+                $CommandItemParameter = $splitCommandItem[0]
+                $CommandItemValue = $splitCommandItem[1]
+                #We Assume if we are doing this we are as deep in the command XML we will go.
+                #So we add a "CommandRoot" attribute to the XML so additional elements know not to go deeper
+                if (!$xmlObject.SelectSingleNode($XPATHCommandRoot)) {
+                    $commandRoot = $XmlObject.SelectSingleNode($XPATHDeepestChildElement)
+                    $commandRoot.setAttribute("CommandRoot","true")
+                } else {
+                    $commandRoot = $xmlObject.SelectSingleNode($XPATHCommandRoot)
+                }
+                $newParam = $commandRoot.AppendChild($xmlObject.CreateElement($commandItemParameter))
+                $newParam.InnerText = $CommandItemValue
+
+                break
+            }
+            $REGEXNameValuePair {
+                #Create a new element and add it to the command root to represent a parameter
+
+                write-debug "Converting NameValuePair: $CommandItem into XML"
+
+                #only split on first equals in case there is one in the value
+                $splitCommandItem = $CommandItem -split '=',2
+                $CommandItemParameter = $splitCommandItem[0]
+                $CommandItemValue = $splitCommandItem[1]
+                #We Assume if we are doing this we are as deep in the command XML we will go.
+                #So we add a "CommandRoot" attribute to the XML so additional elements know not to go deeper
+                if (!$xmlObject.SelectSingleNode($XPATHCommandRoot)) {
+                    $commandRoot = $XmlObject.SelectSingleNode($XPATHDeepestChildElement)
+                    $commandRoot.setAttribute("CommandRoot","true")
+                } else {
+                    $commandRoot = $xmlObject.SelectSingleNode($XPATHCommandRoot)
+                }
+                $newParam = $commandRoot.AppendChild($xmlObject.CreateElement($commandItemParameter))
+                $newParam.InnerText = $CommandItemValue
+
+                break
+            }
+            $REGEXValue {
+                #Adds the text of the value to the deepest XML element
+                write-debug "Converting Value: $CommandItem into XML";
+                #Strip out the quotes
+                $commandItem = $commandItem.replace('"','')
+
+                if ($xmlObject.HasChildNodes) {
+                    $xmlObject.SelectSingleNode($XPATHDeepestChildElement).appendchild($xmlObject.CreateElement($CommandItem)) | out-null
+                } else {
+                    $xmlObject.appendchild($xmlObject.CreateElement($CommandItem)) | out-null
+                }
+                break
+            }
+            $REGEXArgument{
+                #Add a new Child Element to the deepest element
+                write-debug "Converting Argument $CommandItem into XML"
+                if ($xmlObject.HasChildNodes) {
+                    $xmlObject.SelectSingleNode($XPATHDeepestChildElement).appendchild($xmlObject.CreateElement($CommandItem)) | out-null
+                } else {
+                    $xmlObject.appendchild($xmlObject.CreateElement($CommandItem)) | out-null
+                }
+                break
+            }
+            default {throw "Unknown Command Format Syntax"}
         }
+
     }
 
-    end {
-        $xmlObject
-    }
 
+    $xmlObject
 }
